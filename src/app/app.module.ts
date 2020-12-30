bf766218065a41125852f6a26238ad1721ea73a5
Angel-M-R <angel.moreno.romera@bbvaitspain.com>
2020-12-30 13:54:20 +0100

    start

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { MapComponent } from './components/map/map.component';
+import { StopsComponent } from './components/stops/stops.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    MapComponent,
+    StopsComponent
   ],
   imports: [
     BrowserModule,
